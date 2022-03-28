bb96f14c782e00dc3279341de24126301320d6e5
Ben <bgrowcott@hotmail.co.uk>
2022-03-28 19:28:36 +0100

    basic routes set up - models set for posts and users

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
+
+// homepage
 router.get('/', async (req, res) => {
     res.render('homepage');
 });
+
+
+//login
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    // if (req.session.logged_in) {
+    //   res.redirect('/profile');
+    //   return;
+    // }
+  
+    res.render('login');
+  });
 
 module.exports = router;
