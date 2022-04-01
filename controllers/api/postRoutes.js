5e76526e654c58d41760de2e1bcd6a61cc442d66
Ben <bgrowcott@hotmail.co.uk>
2022-04-01 15:15:55 +0100

    got things working on heroku - set up update functionality and put requests

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -9,7 +9,21 @@ router.post('/', withAuth, async (req, res) => {
         ...req.body,
         user_id: req.session.user_id,
       });
-  console.log(newPost)
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.put('/:id', withAuth, async (req, res) => {
+    try {
+        
+      const newPost = await Post.update(req.body, {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
       res.status(200).json(newPost);
     } catch (err) {
       res.status(400).json(err);
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -8,51 +8,85 @@ function showPostModal() {
 $('.cancel-post').click(cancelPost);
 function cancelPost(e) {
   e.preventDefault();
+  updatePost = false
   $('#form-container').css('display', 'none');
 }
 
-// send post request to api
-$('.submit-post').click(sendPost)
+// update posts function
+let updatePost = false;
+let updateId;
+$('.update-post').click((e)=>{
+  updatePost = true
+  updateId = e.target.getAttribute('data-id')
+  $('#title').val($(e.target).parent().parent().siblings('header').children('h3').text())
+  $('#content').val($(e.target).parent().parent().siblings('.card-body').children('p').text())
+  $('#code').val($(e.target).parent().parent().siblings('.card-body').children('pre').children('code').text())
+  showPostModal()
+})
+
+// send post/put request to api
+$('.submit-post').click(sendPost);
 async function sendPost(e) {
-  e.preventDefault();  
+  e.preventDefault();
 
   const title = $('#title').val().trim();
   const content = $('#content').val().trim();
   const code = $('#code').val().trim();
 
-  if (title && content) {
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({ title, content, code }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  if (!updatePost) {
+    if (title && content) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content, code }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    if (response.ok) {
-        cancelPost(e)
-        setTimeout(()=> location.replace('/dashboard'), 200) 
-    } else {
-      alert('Failed to create project');
+      if (response.ok) {
+        cancelPost(e);
+        setTimeout(() => location.replace('/dashboard'), 200);
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  } else {
+    if (title && content) {
+      const id = updateId;
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content, code }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        cancelPost(e);
+        updatePost = false
+        setTimeout(() => location.replace('/dashboard'), 200);
+      } else {
+        alert('Failed to create project');
+      }
     }
   }
 }
 
 // delete post
 
-$('.delete-post').click(deletePost)
+$('.delete-post').click(deletePost);
 
-async function deletePost (e) {
-    if (e.target.hasAttribute('data-id')) {
-      const id = e.target.getAttribute('data-id');
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to delete post');
-      }
+async function deletePost(e) {
+  if (e.target.hasAttribute('data-id')) {
+    const id = e.target.getAttribute('data-id');
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
     }
-  };
+  }
+}
